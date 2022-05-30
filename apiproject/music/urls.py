011b7c74a7699d653403a849ef800939346316be
BatoolBtoush <batoolbtoush98@gmail.com>
2022-05-30 17:42:29 +0300

    fixed docker and it works

--- apiproject/music/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from music.api.viewset import MusicsListAPIView, MusicsDetailAPIView
-
-urlpatterns = [
-    path("api/musics-list", MusicsListAPIView.as_view(), name="music_list"),
-    path("api/<int:pk>", MusicsDetailAPIView.as_view(), name="music_detail"),
-]
